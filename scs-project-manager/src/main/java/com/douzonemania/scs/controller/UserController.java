4222a49f2ce95832ccfb9d497c4f33dda1c05fd1
Eunyeop Kim <dmsduqzz@gmail.com>
2020-05-15 17:30:46 +0900

    회원가입, 회원삭제, 글삭제 팝업추가

--- scs-project-manager/src/main/java/com/douzonemania/scs/controller/UserController.java
+++ scs-project-manager/src/main/java/com/douzonemania/scs/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.douzonemania.scs.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
@@ -46,4 +42,5 @@ public class UserController {
 	public void logout() {
 		
 	}
+
 }
