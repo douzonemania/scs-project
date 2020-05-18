50a445a0466fa991658f6cc0758baccf9f40772e
R35 <xodud7965@naver.com>
2020-05-18 18:20:17 +0900

    DB연동

--- scs-project-shop/src/main/java/com/douzonemania/scs/shop/controller/MainController.java
+++ scs-project-shop/src/main/java/com/douzonemania/scs/shop/controller/MainController.java
@@ -1,13 +1,26 @@
 package com.douzonemania.scs.shop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.douzonemania.scs.shop.service.MainService;
+import com.douzonemania.scs.vo.MemberVo;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private MainService mainService;
+	
 	@RequestMapping({"","/main"})
 	public String home() {
+//		CeoVo vo = new CeoVo();
+//		vo = mainService.getList();
+//		System.out.println(vo);
+		MemberVo vo = new MemberVo();
+		vo = mainService.getList();
+		System.out.println(vo);
 		return "main/index";
 	}
 	
