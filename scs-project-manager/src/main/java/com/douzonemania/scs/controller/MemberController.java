8e4f0743baf598425c609d485cd966b1210b7965
ShinJungEun <sjy8033@naver.com>
2020-05-25 21:24:06 +0900

    email api, 페이징, search 구현 완료

--- scs-project-manager/src/main/java/com/douzonemania/scs/controller/MemberController.java
+++ scs-project-manager/src/main/java/com/douzonemania/scs/controller/MemberController.java
@@ -1,6 +1,6 @@
 package com.douzonemania.scs.controller;
 
-import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,10 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.douzonemania.scs.service.MemberService;
 import com.douzonemania.scs.vo.ceo.CeoVo;
-import com.douzonemania.scs.vo.member.MemberVo;
 import com.douzonemania.security.AuthUser;
 
 // 회원 관리
@@ -22,25 +22,28 @@ public class MemberController {
 	@Autowired
 	MemberService memberService;
 
+	
 	@RequestMapping(value = "/board", method = RequestMethod.GET)
 	public String memberBoard() {
 		return "member/board";
 	}
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String memberList(@AuthUser CeoVo authUser, Model model) {
-//		List<MemberVo> memberList = memberService.memberList(authUser.getId());
-//		for (MemberVo vo : memberList) {
-//			System.out.println("~~~~~~~~"+vo);
-//		}
-//		model.addAttribute("memberList", memberList);
+	@RequestMapping(value= {"", "/list"}, method = { RequestMethod.GET, RequestMethod.POST })
+	public String memberList(@AuthUser CeoVo authUser, Model model,
+			@RequestParam(value="p", required=true, defaultValue="1") int page,
+			@RequestParam(value="kwd", required=true, defaultValue="") String keyword,
+			@RequestParam(value="op", required=true, defaultValue="") String option) {
+		
+		Map<String, Object> map = memberService.find(authUser.getId(), page, keyword, option);
+		
+		model.addAttribute("map", map);
 		
 		return "member/list";
 	}
 	
 	
 	// list에서 메일 발송 버튼을 눌러 전송할 경우(회원 email정보를 받아와야함)
-	@RequestMapping(value = "/email/{no}")
+	@RequestMapping(value = "/email/{no}", method = RequestMethod.GET)
 	public String sendMemberEmail(@PathVariable("no") int no,
 					@AuthUser CeoVo authUser, Model model) {
 		
@@ -51,11 +54,28 @@ public class MemberController {
 		return "member/send-mail";
 	}
 	
-//	// 메일발송 페이지로 가서 메일을 전송할 경우(회원 email을 직접 입력)
-//	@RequestMapping(value = "/email", method = RequestMethod.GET)
-//	public String sendMail(@AuthUser CeoVo authUser, @RequestParam() {
-////		memberService.gmailSend();
-//		return "member/send-mail";
-//	}
+	@RequestMapping(value = "/email/post", method=RequestMethod.POST)
+	public String sendEmail(@AuthUser CeoVo authUser, @RequestParam("memberEmail") String memberEmail,
+			@RequestParam("title") String title, @RequestParam("contents") String contents) {
+		
+		memberService.gmailSend(memberEmail, title, contents);
+		
+		return "member/send-mail-success";
+	}
+	
+	
+	// 메일발송 페이지로 가서 메일을 전송할 경우(회원 email을 직접 입력)
+	@RequestMapping(value = "/email", method = RequestMethod.GET)
+	// @RequestParam
+	public String sendMail(@AuthUser CeoVo authUser) {
+//		memberService.gmailSend();
+		return "member/send-mail";
+	}
+	
+	@RequestMapping(value="/send-mail-success")
+	public String sendEmailSuccess() {
+		
+		return "member/send-mail-success";
+	}
 	
 }
--- scs-project-manager/src/main/java/com/douzonemania/scs/controller/api/MemberController.java
+++ scs-project-manager/src/main/java/com/douzonemania/scs/controller/api/MemberController.java
@@ -2,6 +2,8 @@ package com.douzonemania.scs.controller.api;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,11 +23,28 @@ public class MemberController {
 	private MemberService memberService;
 	
 	
-	@RequestMapping(value="/list", method=RequestMethod.GET)
-	public JsonResult list(@AuthUser CeoVo authUser) {
-		List<MemberVo> memberList = memberService.memberList(authUser.getId());
-		
-		return JsonResult.success(memberList);
-	}
+//	@RequestMapping(value="/list", method=RequestMethod.GET)
+//	public JsonResult list(@AuthUser CeoVo authUser) {
+//		List<MemberVo> memberList = memberService.memberList(authUser.getId());
+//		
+//		return JsonResult.success(memberList);
+//	}
+	
+
+//	@RequestMapping(value="/search", method=RequestMethod.GET)
+//	public JsonResult searchList(@AuthUser CeoVo authUser,
+//			HttpServletRequest request) {
+//		String option = request.getParameter("option");
+//		String input = request.getParameter("input");
+//		
+//		List<MemberVo> memberList 
+//			= memberService.searchMemberList(authUser.getId(), option, input);
+//		
+//		for (MemberVo memberVo : memberList) {
+//			System.out.println(memberVo);
+//		}
+//		
+//		return JsonResult.success(memberList);
+//	}
 	
 }
