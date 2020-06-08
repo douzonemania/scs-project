d34a862dfac334abb1a38803d8f32c5368c9b8ee
R35 <xodud7965@naver.com>
2020-06-08 09:57:40 +0900

    경로수정2

--- scs-project-manager/src/main/java/com/douzonemania/scs/service/ProductService.java
+++ scs-project-manager/src/main/java/com/douzonemania/scs/service/ProductService.java
@@ -31,7 +31,7 @@ public class ProductService {
 	private static final int PAGE_SIZE =5;
 	private static final String SAVE_PATH = "/products";
 	//private static final String SAVE_PATH = "C:\\Users\\bit-user\\git\\scs-project\\scs-project-manager\\src\\main\\webapp\\assets\\images\\scs-uploads";
-	private static final String URL = "/assets/scs-manager/images/";
+	private static final String URL = "/assets/scs-manager/images";
 	
 	@Autowired
 	private ProductRepository productRepository;
