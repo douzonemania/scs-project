40879a914f33c58acde2940aedac96287e9ce787
ShinJungEun <sjy8033@naver.com>
2020-06-09 16:10:47 +0900

    되돌려놓았음

--- scs-project-shop/src/main/java/com/douzonemania/shop/controller/MainController.java
+++ scs-project-shop/src/main/java/com/douzonemania/shop/controller/MainController.java
@@ -1,11 +1,7 @@
 package com.douzonemania.shop.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.douzonemania.shop.service.MainService;
-import com.douzonemania.shop.vo.MemberVo;
 
 @Controller
 @RequestMapping("/{db}")
